d87e917054f3e2feda4ceef80c2f4f02ece2271a
Kiet <5054996+Kietyo@users.noreply.github.com>
2023-05-15 21:58:42 -0700

    Working on implementing layout system. pt. 4

--- korge/src/commonMain/kotlin/com/xenotactic/korge/scenes/TestScene.kt
+++ korge/src/commonMain/kotlin/com/xenotactic/korge/scenes/TestScene.kt
@@ -1,5 +1,6 @@
 package com.xenotactic.korge.scenes
 
+import com.xenotactic.gamelogic.utils.toScale
 import com.xenotactic.korge.ui.Column
 import com.xenotactic.korge.ui.Modifier
 import com.xenotactic.korge.ui.Modifiers
@@ -11,68 +12,73 @@ import korlibs.korge.view.SContainer
 import korlibs.korge.view.container
 import korlibs.korge.view.line
 import korlibs.korge.view.text
+import korlibs.math.geom.Size
 
 class TestScene : Scene() {
 
     override suspend fun SContainer.sceneInit() {
-//        val textBox = this.container {
-//            val text = this.text("This is a sample test")
-//            this.line(
-//                text.x.toDouble(),
-//                text.y.toDouble() + text.scaledHeight,
-//                text.scaledWidth.toDouble(),
-//                text.y.toDouble() + text.scaledHeight
-//            )
-//        }
+        //        val textBox = this.container {
+        //            val text = this.text("This is a sample test")
+        //            this.line(
+        //                text.x.toDouble(),
+        //                text.y.toDouble() + text.scaledHeight,
+        //                text.scaledWidth.toDouble(),
+        //                text.y.toDouble() + text.scaledHeight
+        //            )
+        //        }
 
         val col = Column().addTo(
             this@sceneInit.stage!!,
-            modifiers = Modifiers.with(Modifier.Spacing(start = 10f, between = 20f))
+            modifiers = Modifiers.with(Modifier.Spacing(between = 5f))
         ) {
+            val spacing = Modifier.Spacing(start = 10f, between = 0f)
+
             addLayout {
-                Row(modifiers = Modifiers.with(Modifier.Spacing(between = 10f))) {
-                    addItem { UIText("User Name") }
-                    addItem { UIText("Kills") }
-                    addItem { UIText("Damage") }
+                Row(modifiers = Modifiers.with(spacing)) {
+                    addItem { UIText("User Name", size = Size(100, 18)) }
+                    addItem { UIText("Kills", size = Size(64, 18)) }
+                    addItem { UIText("Damage").apply {
+                        width = 1f
+                    } }
                 }
             }
 
-            //                addLayout {
-            //                    Row(modifiers = Modifiers.with(Modifier.Spacing(start = 10f, between = 20f))) {
-            //                        addItem { Text("Xenotactic") }
-            //                        addItem { Text("13") }
-            //                        addItem { Text("123456") }
-            //                    }
-            //                }
+            addLayout {
+                Row(modifiers = Modifiers.with(spacing)) {
+                    addItem { UIText("Xenotactic", size = Size(100, 18)) }
+                    addItem { UIText("13", size = Size(64, 18)) }
+                    addItem { UIText("123456") }
+                }
+            }
         }
 
+        col.content.scale = 3.toScale()
 
-//        this.sgraphics {
-//            this.stroke(
-//                Colors.WHITE, StrokeInfo(
-//                    thickness = 50.0,
-//                    pixelHinting = true,
-//                    scaleMode = LineScaleMode.NONE,
-//                    lineJoin = LineJoin.ROUND
-//                )
-//            ) {
-//                this.line(
-//                    text.x,
-//                    text.y + text.scaledHeight,
-//                    text.scaledWidth,
-//                    text.y + text.scaledHeight + 1
-//                )
-//            }
-//        }
+        //        this.sgraphics {
+        //            this.stroke(
+        //                Colors.WHITE, StrokeInfo(
+        //                    thickness = 50.0,
+        //                    pixelHinting = true,
+        //                    scaleMode = LineScaleMode.NONE,
+        //                    lineJoin = LineJoin.ROUND
+        //                )
+        //            ) {
+        //                this.line(
+        //                    text.x,
+        //                    text.y + text.scaledHeight,
+        //                    text.scaledWidth,
+        //                    text.y + text.scaledHeight + 1
+        //                )
+        //            }
+        //        }
 
-//        logger.info {
-//            """
-//                text.scaledDimensions(): ${text.scaledDimensions()}
-//                text.unscaledDimensions(): ${text.unscaledDimensions()}
-//            """.trimIndent()
-//        }
+        //        logger.info {
+        //            """
+        //                text.scaledDimensions(): ${text.scaledDimensions()}
+        //                text.unscaledDimensions(): ${text.unscaledDimensions()}
+        //            """.trimIndent()
+        //        }
     }
-
 
     companion object {
 
